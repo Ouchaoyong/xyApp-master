cd2e72848e2ab26b4e735a91080845718b97f71d
Sentox <sentox1990@qq.com>
2016-10-19 19:01:28 +0800

    1

--- mainp/src/com/xy/mainp/base/BaseActivity.java
+++ mainp/src/com/xy/mainp/base/BaseActivity.java
@@ -2,13 +2,13 @@ package com.xy.mainp.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by John on 2016/10/19.
  */
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
