2d85c902ce1fa526c4eae85bae38eaee68fb1eb2
Ouchaoyong <menghua27910746>
2016-10-20 21:44:14 +0800

    2

--- mainp/src/com/xy/mainp/main/WelcomeActivity.java
+++ mainp/src/com/xy/mainp/main/WelcomeActivity.java
@@ -3,7 +3,6 @@ package com.xy.mainp.main;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
 import com.xy.mainp.ActivityWelComeBinding;
@@ -19,48 +18,25 @@ import com.xy.mainp.utils.SystemConfig;
 public class WelcomeActivity extends BaseActivity {
 
     private ActivityWelComeBinding binding;
+    private Animation mAnim;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         StatusBarUtil.setImmersiveStatusBar(this,StatusBarUtil.FULL_SCREEN);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
-
         initView();
     }
 
     private void initView() {
-
         int appVersionCode = SystemConfig.getAppVersionCode(this);
         binding.tvVersionWelcome.setText(String.format(getString(R.string.version_code),appVersionCode+""));
-        initBg();
     }
 
-    private void initBg() {
-        AlphaAnimation anim = new AlphaAnimation(0.1f, 1.0f);
-        anim.setDuration(1000);
-        binding.flContenter.setAnimation(anim);
-        anim.startNow();
-        anim.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
+    @Override
+    public void onEnterAnimationComplete() {
+        super.onEnterAnimationComplete();
 
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
     }
 
-    public boolean isEnter() {
-        showSnakeMsg("welcome to my App!");
-        return true;
-    }
 }
