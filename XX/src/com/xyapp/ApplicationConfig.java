df9a975d2142a424234cc37f99fbc098d3bab13e
Sentox <sentox1990@qq.com>
2016-10-26 21:00:44 +0800

    libs

--- XX/src/com/xyapp/ApplicationConfig.java
+++ XX/src/com/xyapp/ApplicationConfig.java
@@ -23,6 +23,6 @@ public class ApplicationConfig {
     private static void libsInit(Context context){
     //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         //注意该方法要再setContentView方法之前实现
-        SDKInitializer.initialize(context);
+        SDKInitializer.initialize(context.getApplicationContext());
     }
 }
